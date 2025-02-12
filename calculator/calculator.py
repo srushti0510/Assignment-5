13144d902675b93145eda6997cb56d8218311013
srushti0510 <thakresrushti7420@gmail.com>
2025-02-11 21:25:13 -0500

    Implemented simple basic calculator in main branch

---
+++ calculator/calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+    """Simple Calculator with basic operations"""
+
+    @staticmethod
+    def add(a: float, b: float) -> float:
+        return a + b
+
+    @staticmethod
+    def subtract(a: float, b: float) -> float:
+        return a - b
+
+    @staticmethod
+    def multiply(a: float, b: float) -> float:
+        return a * b
+
+    @staticmethod
+    def divide(a: float, b: float) -> float:
+        if b == 0:
+            raise ZeroDivisionError("Cannot divide by zero.")
+        return a / b
